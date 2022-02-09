77fdeb0821c1decd15c4a8db157a758d0f6a54a0
Alan <wutao@qq.com>
2022-02-09 17:25:21 +0800

    fix(全局): 全局样式与全局引入配置

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,16 @@ console.log('fewfew', '所有要');
 </script>
 
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
 	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<style>
+<style lang="stylus">
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
 	margin-top: 60px;
+	color: $brand-color;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+// import path from 'path';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			// styl: {
+			// 	// additionalData: '@import "./src/assets/styles/variables.styl";',
+			// 	// imports: [path.resolve(__dirname, 'src/assets/styles/variables.styl;')],
+
+			// },
+			stylus: {
+				imports: [path.resolve(__dirname, 'src/assets/styles/variables.styl')],
+			},
+		},
+	},
 });
