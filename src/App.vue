02605fe9eec3e49f3b9706a0ae638ec16b7cfaae
Alan <wutao@qq.com>
2022-02-10 14:37:36 +0800

    fix(全局): vue3：语法糖内的defineProps及defineEmits、defineExpose

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue';
-console.log('fewfew', '所有要');
+const callback = (value) => {
+	console.log('fewfew', value);
+};
+const hello = ref(null);
+onMounted(() => {
+	console.log('父级获取子级', hello.value.count);
+});
 </script>
-
 <template>
-	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<HelloWorld ref="hello" msg="Hello Vue 3 + TypeScript + Vite" @callback="callback" />
 </template>
 
 <style lang="stylus">
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,6 +8,7 @@
 	<div>国际化: {{ $t('common.comfirm') }}</div>
 	<div>国际化: {{ i18nText.common.comfirm }}</div>
 	<button @click="switchLang">切换国际化</button>
+	<button @click="callback">子组件向父组件事件传递</button>
 	<hr />
 </template>
 <script setup lang="ts">
@@ -16,8 +17,44 @@ import { useMainStore } from '@/store/main';
 import { useMouse } from '@vueuse/core'; // vueuse插件
 import { useI18n } from 'vue-i18n'; // 国际化
 import { switchLanguage } from '@/locals/index'; // 国际化语言
-// 传参
-defineProps<{ msg: string }>(); // 采用ts专有声明，无默认值
+
+// defineProps获取组件传值
+// 采用ts专有声明，无默认值
+const props = defineProps<{ msg: string }>();
+
+// // 采用ts专有声明，有默认值
+// interface Props {
+// 	msg?: string;
+// }
+// const props = withDefaults(defineProps<Props>(), {
+// 	msg: 'hello1212',
+// });
+// // 非ts专有声明，有默认值
+// const props = defineProps({
+// 	msg: {
+// 		type: String,
+// 		default: 'Hello Alan',
+// 	},
+// });
+// console.log('props', props);
+
+// defineEmits子组件向父组件事件传递
+/*ts专有*/
+const emit = defineEmits<{
+	(e: 'callback', num: number): void;
+}>();
+/*非ts专有*/
+// const emit = defineEmits(['callback']);
+// 事件
+const callback = () => {
+	emit('callback', 2);
+};
+
+// defineExpose组件暴露自己的属性
+const count = ref('组件暴露自己的属性');
+defineExpose({
+	count,
+});
 
 const { x, y } = useMouse();
 // pinia状态管理
